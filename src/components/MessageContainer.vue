15c138c5ef53d19c532d6cf5d2fbf61dd3dad04f
thasup <thanachonfirst@hotmail.com>
2022-04-25 23:10:53 +0700

    Learn about stubbing child components

---
+++ src/components/MessageContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <MessageDisplay />
+  </div>
+</template>
+
+<script>
+import MessageDisplay from '@/components/MessageDisplay.vue';
+
+export default {
+  components: {
+    MessageDisplay,
+  },
+};
+</script>
